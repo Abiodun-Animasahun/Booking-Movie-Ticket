63ff210d1d49b91eec563f77bd4ced938d9bda26
Abiodun <abiodun.animasahun2014@gmail.com>
2023-09-24 09:09:54 +0100

    Create and Read done

--- action.js
+++ action.js
@@ -1,54 +1,89 @@
+/* JavaScript to handle adding, removing, marking tasks as complete, and local storage */
+
 document.addEventListener("DOMContentLoaded", function () {
   const taskInput = document.getElementById("taskInput");
-  const inputField = document.getElementById("inputField");
+  const addTaskButton = document.getElementById("addTaskButton");
   const taskList = document.getElementById("taskList");
 
-  // Initialize tasks (simulated database)
-  let tasks = [];
+  // code to retrieve tasks from localStorage
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  // Function to display tasks
-  function displayTasks() {
-    taskList.innerHTML = "";
-    tasks.forEach((task, index) => {
-      const li = document.createElement("li"); //addtaskBtn
-      li.innerHTML = `
-              ${task}
-              <button class="editBtn">Edit</button>
-              <button class="deleteBtn">Delete</button>
-          `;
+  // Function to update the task list
+  function updateTaskList() {
+    taskList.innerHTML = ""; // Clear the current task list
 
-      // Edit task
-      const editBtn = li.querySelector(".editBtn");
+    // Iterate through saved tasks and add them to the list
+    savedTasks.forEach(function (task) {
+      const taskItem = document.createElement("p");
+      taskItem.innerHTML = `
+          
+          <img class="task-image" src="Resources/Ellipse 1.svg" />
+          <label>${task.text}</label>
+          
+         
+        `;
+
+      // The event listener to delete the task when the delete button is clicked
+      /*
+      taskItem
+        .querySelector("button.delete-button")
+        .addEventListener("click", function () {
+          const index = savedTasks.findIndex(
+            (savedTask) => savedTask.text === task.text
+          );
+          if (index !== -1) {
+            savedTasks.splice(index, 1); // Remove the task from the savedTasks array
+            localStorage.setItem("tasks", JSON.stringify(savedTasks)); // Update localStorage
+            updateTaskList(); // Update the displayed task list
+          }
+        });
+        */
+
+      // edit
+      /*  const editBtn = li.querySelector(".editBtn");
       editBtn.addEventListener("click", () => {
         const updatedTask = prompt("Edit task:", task);
         if (updatedTask !== null) {
-          tasks[index] = updatedTask;
-          displayTasks();
+          task[index] = updatedTask;
+          updateTaskList();
         }
       });
+      */
 
-      // Delete task
-      const deleteBtn = li.querySelector(".deleteBtn");
-      deleteBtn.addEventListener("click", () => {
-        tasks.splice(index, 1);
-        displayTasks();
-      });
+      // use event listener to mark the task as complete
+      /* taskItem
+        .querySelector("input[type='checkbox']")
+        .addEventListener("change", function () {
+          taskItem.querySelector("label").style.textDecoration = this.checked
+            ? "line-through"
+            : "none";
+          task.isComplete = this.checked;
+          localStorage.setItem("tasks", JSON.stringify(savedTasks)); // Update localStorage
+        });
+        
 
-      taskList.appendChild(li);
+      // Initialize the checkbox state and label style based on saved data
+      if (task.isComplete) {
+        taskItem.querySelector("input[type='checkbox']").checked = true;
+        taskItem.querySelector("label").style.textDecoration = "line-through";
+      }
+      */
+
+      taskList.appendChild(taskItem);
     });
   }
 
-  // Add task
+  // Initial update of the task list
+  updateTaskList();
 
-  inputField.addEventListener("keypress", (e) => {
-    const newTask = taskInput.value.trim();
-    if (e.key === "Enter") {
-      tasks.push(newTask);
-      taskInput.value = "";
-      displayTasks();
+  // Event listener to add a new task
+  addTaskButton.addEventListener("click", function () {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+      savedTasks.push({ text: taskText, isComplete: false }); // Add the new task to the savedTasks array
+      localStorage.setItem("tasks", JSON.stringify(savedTasks)); // Update localStorage
+      updateTaskList(); // Update the displayed task list
+      taskInput.value = ""; // Clear the input field
     }
   });
-
-  // Initial display
-  displayTasks();
 });
